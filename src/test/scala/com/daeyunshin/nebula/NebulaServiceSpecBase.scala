9a21df17aa6c572b3cc3cc5a9a8b122def07be7b
Daeyun Shin <daeyun@daeyunshin.com>
2014-08-15 03:52:28 -0700

    Select a random port in tests

--- src/test/scala/com/daeyunshin/nebula/NebulaServiceSpecBase.scala
+++ src/test/scala/com/daeyunshin/nebula/NebulaServiceSpecBase.scala
@@ -5,6 +5,7 @@ import java.util.concurrent.Executors
 
 import com.daeyunshin.nebula.graph.DirectedGraphOperation
 import com.daeyunshin.nebula.service.{NebulaService, NebulaServiceImpl}
+import com.daeyunshin.nebula.utils.PortFinder
 import com.twitter.cassovary.util.io.AdjacencyListGraphReader
 import com.twitter.finagle.Thrift
 import com.twitter.finagle.builder.{Server, ServerBuilder}
@@ -12,16 +13,18 @@ import com.twitter.finagle.thrift.ThriftServerFramedCodec
 import org.apache.thrift.protocol.TBinaryProtocol
 import org.scalatest.FlatSpec
 
+import scala.util.control.Breaks._
+
 /**
  * Created by daeyun on 8/14/14.
  */
 class NebulaServiceSpecBase extends FlatSpec {
   val SampleGraphPath = "src/test/resources"
   val SampleGraphFilePrefix = "adj_1"
-  val ThriftPort = 42101
   val ThriftServiceName = "NebulaServiceSpec"
 
   var server: Server = null
+  var port: Int = 0
 
   def startThriftServer() {
     val graph = AdjacencyListGraphReader.forIntIds(
@@ -38,11 +41,23 @@ class NebulaServiceSpecBase extends FlatSpec {
       new TBinaryProtocol.Factory()
     )
 
-    server = ServerBuilder()
-      .bindTo(new InetSocketAddress(ThriftPort))
-      .codec(ThriftServerFramedCodec())
-      .name(ThriftServiceName)
-      .build(service)
+    breakable {
+      for (i <- 1 to 5) {
+        port = PortFinder.findFreePort()
+
+        try {
+          server = ServerBuilder()
+            .bindTo(new InetSocketAddress(port))
+            .codec(ThriftServerFramedCodec())
+            .name(ThriftServiceName)
+            .build(service)
+          break
+        } catch {
+          case e: Exception =>
+            e.printStackTrace()
+        }
+      }
+    }
   }
 
   def stopThriftServer(): Unit = {
@@ -54,6 +69,6 @@ class NebulaServiceSpecBase extends FlatSpec {
   }
 
   def getThriftClient() = {
-    Thrift.newIface[NebulaService.FutureIface]("localhost:%d" format ThriftPort)
+    Thrift.newIface[NebulaService.FutureIface]("localhost:%d" format port)
   }
 }
